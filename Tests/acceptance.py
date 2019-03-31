746a10fec2cb551e1084dcf7ef2c54a723a8f654
yosikadosh <kadosh01@gmail.com>
2019-03-31 20:50:44 +0300

    Tests

--- Tests/acceptance.py
+++ Tests/acceptance.py
@@ -1,17 +1,28 @@
-from django.test import TestCase
-from Domain import SystemManager
+import unittest
+from Service.service import SystemInterface
 
 
-def return_true():
-    return True
+class TestStringMethods(unittest.TestCase):
 
-
-def return_false():
-    return False
-
-
-class InitTestCase(TestCase):
+    service = SystemInterface()
 
     def setUp(self):
+        manager = {'name': 'mana', 'password': '123456'}
+        manager["name"]
 
-       # manager = SystemManager("mana", "123456")
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
+
+if __name__ == '__main__':
+    unittest.main()
