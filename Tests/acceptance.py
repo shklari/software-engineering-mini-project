05334d96b8f899bbe8b1d261158a0e839d1c7656
itays <shklari@post.bgu.ac.il>
2019-03-31 19:55:44 +0300

    acceptance file (created)

--- Tests/acceptance.py
+++ Tests/acceptance.py
@@ -1,4 +1,22 @@
-#from django.test import TestCase
+from django.test import TestCase
+from Domain import SystemManager
 
-#class InitTestCase(TestCase):
-#    def setUp(self):
+
+def return_true():
+    return True
+
+
+def return_false():
+    return False
+
+
+flag1 = 0
+
+flag2 = 0
+
+flag3 = 0
+
+
+class InitTestCase(TestCase):
+    def setUp(self):
+        manager = SystemManager("mana", "123456")
