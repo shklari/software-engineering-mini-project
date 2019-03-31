a8e939659cd1bed4922d10d154ef0c7d86d864ee
itays <shklari@post.bgu.ac.il>
2019-03-31 20:09:34 +0300

    acceptance file (created)

--- Tests/acceptance.py
+++ Tests/acceptance.py
@@ -10,13 +10,8 @@ def return_false():
     return False
 
 
-flag1 = 0
-
-flag2 = 0
-
-flag3 = 0
-
-
 class InitTestCase(TestCase):
+
     def setUp(self):
+
         manager = SystemManager("mana", "123456")
