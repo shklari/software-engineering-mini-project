0f1a67dc663ca04b96199f7568bf77bd0ef8b1e8
itays <shklari@post.bgu.ac.il>
2019-03-31 19:41:46 +0300

    systemManager and TestsPackage (created)

---
+++ Domain/SystemManager.py
@@ -0,0 +1,9 @@
+from Domain import Client
+
+
+class SystemManager(Client):
+
+    def _init_(self, new_name, new_password):
+        super(new_name, new_password)
+
+
---
+++ Tests/acceptance.py
@@ -0,0 +1,4 @@
+from django.test import TestCase
+
+class InitTestCase(TestCase):
+    def setUp(self):
