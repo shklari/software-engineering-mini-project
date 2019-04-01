84200bd68067907cfeba88d2f38b7be57f34238a
Inbar Naus <naus@post.bgu.ac.il>
2019-04-01 23:28:53 +0300

    client user

--- Domain/Store.py
+++ Domain/Store.py
@@ -1,24 +1,29 @@
+from typing import Dict, Any
+from .Item import Item
 from .StoreOwner import StoreOwner
 from .DiscountPolicy import DiscountPolicy
 from .ProcurementPolicy import ProcurementPolicy
 from .User import User
+from .StoreManager import StoreManager
 
 
 # Interface
 class Store(object):
 
+    # inventory: Dict[Item, Any]
+
     def __init__(self, name, rank):
         self.name = name
         self.rank = rank
-        self.inventory = dict.fromkeys(['item_name', 'quantity'])
+        self.inventory = {}
         self.storeOwners = []
         self.storeManagers = []
         self.discountPolicy = 0
         self.procPolicy = 0
 
-    def set_policy(self, new_policy):
+    def set_proc_policy(self, new_policy):
         if isinstance(new_policy, ProcurementPolicy):
-            self.procPolicy = new_policy;
+            self.procPolicy = new_policy
             print("new policy has updated")
             return True
         print("illegal policy")
@@ -31,6 +36,13 @@ class Store(object):
                     return True
         return False
 
+    def check_if_store_manager(self, user):
+        if isinstance(user, User):
+            for k in self.storeManagers:
+                if k.username == user.username:
+                    return True
+        return False
+
     # 4.1.1
     def add_item_to_inventory(self, user, item, quantity):
         if isinstance(user, User) and user.logged_in:
@@ -39,13 +51,13 @@ class Store(object):
                     self.inventory[item] += quantity
                 else:
                     self.inventory[item] = quantity
-                print("item has been successesfuly added to the store inventory!")
+                print("item has been successfully added to the store inventory!")
                 return True
             else:
                 print("user is no store owner for this store")
                 return False
         else:
-            print("user is not logged in or not a store owner at all")
+            print("user is not logged in or not a store owner")
             return True
 
     # 4.1.2
@@ -67,7 +79,7 @@ class Store(object):
                 print("user is no store owner for this store")
                 return False
         else:
-            print("user is not logged in or not a store owner at all")
+            print("user is not logged in or not a store owner")
             return True
 
     # 4.1.3
@@ -88,10 +100,10 @@ class Store(object):
                 print("user is no store owner for this store")
                 return False
         else:
-            print("user is not logged in or not a store owner at all")
+            print("user is not logged in or not a store owner")
             return True
 
-    def set_policy(self, new_policy):
+    def set_discount_policy(self, new_policy):
         if isinstance(new_policy, DiscountPolicy):
             self.discountPolicy = new_policy
             print("new discount policy has updated")
@@ -102,10 +114,13 @@ class Store(object):
 
     # 4.3
     def add_new_owner(self, owner, new_owner):
-        if isinstance(owner, StoreOwner) and owner.logged_in:
-            if owner in self.storeOwners:
-                if new_owner not in self.storeOwners:
-                    self.storeOwners.append(self, new_owner)
+        if isinstance(owner, User) and owner.logged_in:
+            if self.check_if_store_owner(owner):
+                if not self.check_if_store_owner(new_owner):
+                    self.storeOwners.append(StoreOwner(new_owner.username, new_owner.password, owner))
+                    for k in self.storeOwners:
+                        if k.username == owner.username:
+                            k.add_appointee(new_owner)
                     print("new store owner has been added successfully!")
                     return True
                 else:
@@ -115,14 +130,71 @@ class Store(object):
                 print("user is no store owner for this store")
                 return False
         else:
-            print("user is not logged in or not a store owner at all")
+            print("user is not logged in or not a store owner")
             return True
 
     # 4.4
-    def remove_owner(self, owner, owner_to_remove): pass
+    def remove_owner(self, owner, owner_to_remove):
+        if isinstance(owner, User) and owner.logged_in:
+            if self.check_if_store_owner(owner):
+                if self.check_if_store_owner(owner_to_remove):
+                    for k in self.storeOwners:
+                        if k.username == owner.username:
+                            if k.is_appointee(owner_to_remove):
+                                for x in self.storeOwners:
+                                    if x.username == owner_to_remove.username:
+                                        for i in x.get_appointees():
+                                            self.remove_owner(owner_to_remove, i)
+                                    self.storeOwners.remove(x)
+                                k.remove_appointee(owner_to_remove)
+                                print("store owner has been removed successfully!")
+                                return True
+                            print("the owner is not the appointer for this owner")
+                            return False
+                else:
+                    print("user is not an owner of this store")
+                    return False
+            else:
+                print("user is no store owner for this store")
+                return False
+        else:
+            print("user is not logged in or not a store owner")
+            return True
 
     # 4.5
-    def add_new_manager(self, owner, new_manager): pass
+    def add_new_manager(self, owner, new_manager):
+        if isinstance(owner, User) and owner.logged_in:
+            if self.check_if_store_owner(owner):
+                if not self.check_if_store_manager(new_manager):
+                    self.storeManagers.append(StoreManager(new_manager.username, new_manager.password, owner))
+                    for k in self.storeManagers:
+                        if k.username == owner.username:
+                            k.add_appointee(new_manager)
+                    print("new store manager has been added successfully!")
+                    return True
+                else:
+                    print("user is already a manager of this store")
+                    return False
+            else:
+                print("user is no store owner for this store")
+                return False
+        else:
+            print("user is not logged in or not a store owner")
+            return True
+
+    def set_permissions_to_manager(self, owner, manager, permissions):
+        for k in self.storeOwners:
+            if k.username == owner.username:
+                if k.is_appointee(manager):
+                    for x in self.storeManagers:
+                        if x.username == manager.username:
+                            x.set_permissions(permissions)
+                            print("permissions for manager has updated")
+                            return True
+                print("owner is not the manager appointer")
+                return False
+            print("user is not a store owner")
+            return False
 
     # 4.6
     def remove_manager(self, owner, manager_to_remove): pass
