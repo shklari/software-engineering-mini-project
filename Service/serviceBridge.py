ea31c1be4ffb03770140b09cc54fff9c49a49316
yosikadosh <kadosh01@gmail.com>
2019-04-01 19:51:20 +0300

    service

---
+++ Service/serviceBridge.py
@@ -0,0 +1,66 @@
+from Service.service import ServiceInterface
+
+
+class ServiceBridge(ServiceInterface):
+    def init(self, system_manager, collecting, supplying, integrity):
+        pass
+
+    def sign_up(self, username, password):
+        pass
+
+    def login(self, username, password):
+        pass
+
+    def search(self, param, ):
+        pass
+
+    def buy_items(self, items):
+        pass
+
+    def logout(self):
+        pass
+
+    def create_store(self, name):
+        pass
+
+    def remove_client(self, client):
+        pass
+
+    def add_to_cart(self, store, items):
+        pass
+
+    def edit_cart(self, cart, params):
+        pass
+
+    def buy_item(self, item):
+        pass
+
+    def remove_owner(self, owner):
+        pass
+
+    def remove_manager(self, manager):
+        pass
+
+    def add_item_to_inventory(self, store, item, quantity):
+        pass
+
+    def remove_item_from_inventory(self, store, item, quantity):
+        pass
+
+    def edit_item_price(self, item, new_price):
+        pass
+
+    def add_new_owner(self, owner, new_owner):
+        pass
+
+    def add_new_manager(self, owner, new_manager):
+        pass
+
+    def set_price(self, new_price):
+        pass
+
+    def collect(self, amount, credit_details):
+        pass
+
+    def get_supply(self, user, items):
+        pass
