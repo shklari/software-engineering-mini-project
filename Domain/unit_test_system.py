9396494e7804e6ace2345850744b77cf6387ca57
shaioz <shaioz2005@gmail.com>
2019-04-02 22:51:36 +0300

    add get cur user to system

--- Domain/unit_test_system.py
+++ Domain/unit_test_system.py
@@ -1,9 +1,25 @@
 from unittest import TestCase
+from Domain.System import System
 
 
 class UnitTestSystem(TestCase):
+    def setUp(self):
+        self.system = System()
+
+
+class TestInit(UnitTestSystem):
     def test_init_system(self):
-        self.fail()
+        self.assertEqual(self.system.system_manager, 0)
+        self.assertEqual(self.system.cur_user, 0)
+        self.assertDictEqual(self.system.users, {})
+        self.assertEqual(self.system.stores, [])
+        self.assertEqual(self.system.init_system('shaioz', None), None)
+        self.system.init_system('shaioz', 1234)
+        self.assertEqual(self.system.system_manager.username, 'shaioz')
+        self.assertGreater(len(self.system.users), 0)
+
+
+class TestUser(UnitTestSystem):
 
     def test_sign_up(self):
         self.fail()
@@ -14,8 +30,10 @@ class UnitTestSystem(TestCase):
     def test_logout(self):
         self.fail()
 
-    def test_create_store(self):
+    def test_remove_user(self):
         self.fail()
 
-    def test_remove_user(self):
+
+class TestStore(UnitTestSystem):
+    def test_create_store(self):
         self.fail()
