b263902ff420e9e5f32022cb03c0a0724200487a
Inbar Naus <naus@post.bgu.ac.il>
2019-04-03 01:59:20 +0300

    client user

--- Domain/StoreManager.py
+++ Domain/StoreManager.py
@@ -1,11 +1,12 @@
-
+from .User import User
 
 #interface
-class StoreManager (object):
+class StoreManager (User):
 
-    def __init__(self, username, password, appointer, permissions = {'Edit': False, 'Remove': False, 'Add': False}):
+    def __init__(self, username, password, appointer, permissions=None):
         super(StoreManager, self).__init__(username, password)
         self.permissions = permissions
         self.appointer = appointer
-        self.permissions = permissions
+        if permissions is None:
+            self.permissions = {'Edit': False, 'Remove': False, 'Add': False}
 
