463ce4f13b5afc936bef5c8fd21574da8220f5f7
shaioz <shaioz2005@gmail.com>
2019-04-03 01:40:12 +0300

    system fix

--- Domain/Cart.py
+++ Domain/Cart.py
@@ -7,8 +7,8 @@ class Cart:
         self.store_name = store_name
         self.items_and_quantities = {}
 
-    def add_item_to_cart(self, store, item_name, quantity):
-        if not self.get_item_if_available(item_name, store):
+    def add_item_to_cart(self, item_name, quantity):
+        if not self.get_item_if_available(item_name):
             return False
         if item_name in self.items_and_quantities:
             self.items_and_quantities[item_name] += quantity
@@ -16,9 +16,15 @@ class Cart:
             self.items_and_quantities[item_name] = quantity
         return True
 
-    def get_item_if_available(self, item_name, store):
-        if item_name in store.inventory and store.inventory[item_name] > 0:
-            return store.inventory[item_name]
+    def get_item_if_available(self, item_name):
+        my_store = System.get_store(self.store_name)
+        bol = False
+        for k in my_store.inventory:
+            if k['name'] == my_store.name:
+                if k['quantity'] > 0:
+                    bol = True
+        if my_store.search_item_by_name(item_name) and bol:
+            return my_store.search_item_by_name(item_name)
         return False
 
     def remove_item_from_cart(self, item_name):
