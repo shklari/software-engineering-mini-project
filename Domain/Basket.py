95d534aa8c86d720f956465be910d0348029df8f
avabe <avabe@post.bgu.ac.il>
2019-04-02 21:38:21 +0300

    implemented more functions in serviceImpl

--- Domain/Basket.py
+++ Domain/Basket.py
@@ -9,13 +9,14 @@ class Basket:
     def get_carts(self):
         return self.carts
 
-    def get_cart_by_store(self, storeNmae):
+    def get_cart_by_store(self, storeName):
         for cart in self.carts:
-            if cart.get_store_name() == storeNmae: return cart
+            if cart.get_store_name() == storeName:
+                return cart
         return False
 
-    def remove_item_from_cart(self, storeNmae, item):
-        cart = self.get_cart_by_store(storeNmae)
+    def remove_item_from_cart(self, storeName, item):
+        cart = self.get_cart_by_store(storeName)
         cart.remove_item_from_cart(item)
 
 
