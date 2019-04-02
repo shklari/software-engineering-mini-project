b78047316c6979557d6f8612ef5909e79e6e16d3
yosikadosh <kadosh01@gmail.com>
2019-04-02 20:33:15 +0300

    basket , cart , Guest

--- Service/serviceBridge.py
+++ Service/serviceBridge.py
@@ -1,69 +1,84 @@
 from Service.service import ServiceInterface
+from Service.serviceImpl import ServiceImpl
 
 
 class ServiceBridge(ServiceInterface):
-    def get_cart(self, store):
+    real = None
+
+    def __init__(self):
+        self.real = ServiceImpl()
+
+    def remove_from_cart(self, store_name, item):
         pass
+
+    def get_item_from_cart(self, cart, item_name):
+        pass
+
+    def remove_user(self, username):
+        return True if self.real is None else self.real.remove_user(username)
+
+    def get_cart(self, store):
+        return True if self.real is None else self.real.get_cart(store)
 
     def init(self, sm_username, sm_password):
-        pass
+        return True if self.real is None else self.real.init(sm_username, sm_password)
 
     def sign_up(self, username, password):
-        pass
+        return True if self.real is None else self.real.sign_up(username, password)
 
     def login(self, username, password):
-        pass
+        return True if self.real is None else self.real.login(username, password)
 
-    def search(self, param, ):
-        pass
+    def search(self, param):
+        return True if self.real is None else self.real.search(param)
 
     def buy_items(self, items):
-        pass
+        return True if self.real is None else self.real.buy_items(items)
 
     def logout(self):
-        pass
+        return True if self.real is None else self.real.logout()
 
     def create_store(self, name):
-        pass
+        return True if self.real is None else self.real.create_store(name)
 
-    def remove_client(self, client):
-        pass
+    def remove_user(self, user):
+        return True if self.real is None else self.real.remove_client(user)
 
     def add_to_cart(self, store, items):
-        pass
+        return True if self.real is None else self.real.add_to_cart(store, items)
 
     def edit_cart(self, cart, params):
-        pass
+        return True if self.real is None else self.real.edit_cart(cart, params)
 
     def buy_item(self, item):
-        pass
+        return True if self.real is None else self.real.buy_item(item)
 
     def remove_owner(self, owner):
-        pass
+        return True if self.real is None else self.real.remove_owner(owner)
 
     def remove_manager(self, manager):
-        pass
+        return True if self.real is None else self.real.remove_manager(manager)
 
     def add_item_to_inventory(self, item, store, quantity):
-        pass
+        return True if self.real is None else self.real.add_item_to_inventory(item, store, quantity)
 
     def remove_item_from_inventory(self, item, store, quantity):
-        pass
+        return True if self.real is None else self.real.remove_item_from_inventory(item, store, quantity)
 
     def edit_item_price(self, item, new_price):
-        pass
+        return True if self.real is None else self.real.edit_item_price(item, new_price)
 
     def add_new_owner(self, new_owner):
-        pass
+        return True if self.real is None else self.real.add_new_owner(new_owner)
 
     def add_new_manager(self, new_manager):
-        pass
+        return True if self.real is None else self.real.add_new_manager(new_manager)
 
     def set_price(self, new_price):
-        pass
+        return True if self.real is None else self.real.set_price(new_price)
 
     def collect(self, amount, credit_details):
-        pass
+        return True if self.real is None else self.real.collect(amount, credit_details)
 
     def get_supply(self, user, items):
-        pass
+        return True if self.real is None else self.real.get_supply(user, items)
