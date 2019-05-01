a37daa3c57104607a4eadaa4b75420166fa5e8be
shaioz <shaioz2005@gmail.com>
2019-05-01 19:23:15 +0300

    cart guest and system

--- Domain/ExternalSystems.py
+++ Domain/ExternalSystems.py
@@ -1,24 +1,77 @@
 # Interface
 class CollectingSystem(object):
 
-    # 7
-    @staticmethod
-    def collect(amount, credit_details):
-        if amount > credit_details.balance:
+    def __init__(self):
+        self.flag = 0
+
+    def switch(self):
+        if self.flag == 0:
+            self.flag = 1
+        else:
+            self.flag = 0
+
+    def init(self):
+        if self.flag == 0:
             return False
-        return True
+        else:
+            return True
+
+    def collect(self):
+        return self.init()
+
+    # 7
+    # @staticmethod
+    # def collect(amount, credit_details):
+    #     if amount > credit_details.balance:
+    #         return False
+    #     return True
 
 
 # Interface
 class SupplyingSystem(object):
 
+    def __init__(self):
+        self.flag = 0
+
+    def switch(self):
+        if self.flag == 0:
+            self.flag = 1
+        else:
+            self.flag = 0
+
+    def init(self):
+        if self.flag == 0:
+            return False
+        else:
+            return True
+
+    def get_supply(self):
+        return self.init()
+
     # 8
-    @staticmethod
-    def get_supply(user, items):
-        return True
+    # @staticmethod
+    # def get_supply(user, items):
+    #    return True
 
 
 # Interface
 class TraceabilitySystem(object):
 
-    def trace(self, item, store): pass
+    def __init__(self):
+        self.flag = 1
+
+    def switch(self):
+        if self.flag == 0:
+            self.flag = 1
+        else:
+            self.flag = 0
+
+    def init(self):
+        if self.flag == 0:
+            return False
+        else:
+            return True
+
+    def trace(self, item, store):
+        return self.init()
+
