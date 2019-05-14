f84ddca38010c3941457bd4e14af9b9444b935ae
itays <shklari@post.bgu.ac.il>
2019-05-14 20:41:34 +0300

    buying policy

---
+++ Tests/UnitTests/BuyingPolicyUnitTests.py
@@ -0,0 +1,11 @@
+import unittest
+
+from Service.serviceBridge import ServiceBridge
+from Domain.BuyingPolicy import *
+
+class
+
+class TestStore(unittest.TestCase):
+
+    service = ServiceBridge()
+
