02b8053f3e921f3326227d2d22d2398d5d1ad613
yosikadosh <kadosh01@gmail.com>
2019-05-14 20:57:32 +0300

    client

--- store/urls.py
+++ store/urls.py
@@ -1,7 +1,8 @@
+from django.contrib.auth.views import LoginView
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
 ]
