96b838cc448a48b92de2a93c06a9ac759127ac08
itays <shklari@post.bgu.ac.il>
2019-05-14 19:53:06 +0300

    sever

--- store/urls.py
+++ store/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from . import views
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('create_store/', LoginView.as_view(template_name='store/create_store.html'), name="create_store"),
 ]
