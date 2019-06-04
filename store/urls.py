185c778d7171f58df71384a12cb66ce798df709e
yosikadosh <kadosh01@gmail.com>
2019-06-04 23:46:01 +0300

    update

--- store/urls.py
+++ store/urls.py
@@ -13,5 +13,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('single_product/', LoginView.as_view(template_name='store/single_product.html'), name="single_product"),
     path('shop_all/', LoginView.as_view(template_name='store/shop_all.html'), name="shop_all"),
+    path('add_product/', LoginView.as_view(template_name='store/add_product.html'), name="add_product"),
     path('store/', LoginView.as_view(template_name='store/store.html'), name="store"),
 ]+ static(settings.STATIC_ROOT, document_root=settings.STATIC_ROOT)
