b991859b10b9e127cc7891068b1e5ff249153e1b
yosikadosh <kadosh01@gmail.com>
2019-06-11 16:41:27 +0300

    System , service

--- static/static_dirs/js/client.js
+++ static/static_dirs/js/client.js
@@ -4,7 +4,7 @@ function send_msg(msg, onsuccess , onfailure)
 {
 
     try {
-        websocket = new WebSocket("ws://192.168.0.22:6789");
+        websocket = new WebSocket("ws://localhost:6789");
         //websocket = new WebSocket("ws://192.168.0.53:6789");
         console.info("try to send ....");
         var json = JSON.stringify(msg);
@@ -16,7 +16,7 @@ function send_msg(msg, onsuccess , onfailure)
         websocket.onmessage = function (event) {
 
             data = JSON.parse(event.data);
-            console.log("bbbbbbbbbbbbbbbb " + data);
+            //console.log("bbbbbbbbbbbbbbbb " + data);
             switch (data.action) {
                 case 'notify' :
                     alert(data.message);
