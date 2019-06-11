1873b922c1e9bd99f3402db6b0524a38824bb6eb
Inbar Naus <naus@post.bgu.ac.il>
2019-06-11 21:48:18 +0300

    Merge branch 'Version_2' of https://github.com/kadosh01/Software_Engineering_Project_2019- into Version_2
    # Conflicts:
    #	static/static_dirs/js/client.js

--- Domain/System.py
+++ Domain/System.py
@@ -6,6 +6,7 @@ from Domain.Response import ResponseObject
 from Domain.SystemManager import SystemManager
 from passlib.hash import pbkdf2_sha256
 from log.Log import Log
+from DataAccess import sqlite_database
 
 import functools
 
@@ -16,6 +17,7 @@ class System:
     def __init__(self):
         self.user_types = {"1": "guest", "2": "user", "3": "store_owner", "4": "store_manager", "5": "sys_manager"}
         self.system_manager = None
+        self.database = sqlite_database
         self.cur_user = None
         self.users = {}  # {username, user}
         self.stores = []
@@ -53,6 +55,7 @@ class System:
         else:
             enc_password = pbkdf2_sha256.hash(password)
             new_user = User(username, enc_password, age, country)
+            self.database.add_to_users(new_user)
             self.users[username] = new_user
             self.log.set_info("signup succeeded", "eventLog")
             return ResponseObject(True, True, "Welcome new user " + username + "! You may now log in")
